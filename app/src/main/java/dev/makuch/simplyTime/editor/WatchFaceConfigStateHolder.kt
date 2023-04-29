74747ca79ac685c524a6b0d1864ac914ab1a228c
Tadeusz Makuch <tadeusz.makuch@gmail.com>
2023-04-29 22:24:46 +0200

    simplified editor start event

--- app/src/main/java/dev/makuch/simplyTime/editor/WatchFaceConfigStateHolder.kt
+++ app/src/main/java/dev/makuch/simplyTime/editor/WatchFaceConfigStateHolder.kt
@@ -1,47 +1,21 @@
-/*
- * Copyright 2021 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package dev.makuch.simplyTime.editor
 
-import android.graphics.Bitmap
-import android.graphics.Color
 import android.util.Log
 import androidx.activity.ComponentActivity
-import androidx.wear.watchface.DrawMode
-import androidx.wear.watchface.RenderParameters
-import androidx.wear.watchface.complications.data.ComplicationData
 import androidx.wear.watchface.editor.EditorSession
 import androidx.wear.watchface.style.UserStyle
 import androidx.wear.watchface.style.UserStyleSchema
 import androidx.wear.watchface.style.UserStyleSetting
-import androidx.wear.watchface.style.WatchFaceLayer
-
 import dev.makuch.simplyTime.utils.COMPLICATION_ID
 import dev.makuch.simplyTime.utils.SHOW_DIVISION_RING_SETTING
-
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.plus
-import kotlinx.coroutines.yield
 
 class WatchFaceConfigStateHolder(
     private val scope: CoroutineScope,
@@ -49,7 +23,6 @@ class WatchFaceConfigStateHolder(
 ) {
     private lateinit var editorSession: EditorSession
 
-    // Keys from Watch Face Data Structure
     private lateinit var showDivisionRing: UserStyleSetting.BooleanUserStyleSetting
 
     val uiState: StateFlow<EditWatchFaceUiState> =
@@ -60,17 +33,13 @@ class WatchFaceConfigStateHolder(
 
             extractsUserStyles(editorSession.userStyleSchema)
 
-            emitAll(
-                combine(
-                    editorSession.userStyle,
-                    editorSession.complicationsPreviewData
-                ) { userStyle, complicationsPreviewData ->
-                    yield()
+            editorSession.userStyle.collect { userStyle ->
+                emit(
                     EditWatchFaceUiState.Success(
-                        createWatchFacePreview(userStyle, complicationsPreviewData)
+                        getConfigData(userStyle)
                     )
-                }
-            )
+                )
+            }
         }
             .stateIn(
                 scope + Dispatchers.Main.immediate,
@@ -89,25 +58,10 @@ class WatchFaceConfigStateHolder(
         }
     }
 
-    private fun createWatchFacePreview(
-        userStyle: UserStyle,
-        complicationsPreviewData: Map<Int, ComplicationData>
+    private fun getConfigData(
+        userStyle: UserStyle
     ): UserStylesAndPreview {
         Log.d(TAG, "updatesWatchFacePreview()")
-
-        val bitmap = editorSession.renderWatchFaceToBitmap( //TODO drop this
-            RenderParameters(
-                DrawMode.INTERACTIVE,
-                WatchFaceLayer.ALL_WATCH_FACE_LAYERS,
-                RenderParameters.HighlightLayer(
-                    RenderParameters.HighlightedElement.AllComplicationSlots,
-                    Color.RED, // Red complication highlight.
-                    Color.argb(128, 0, 0, 0) // Darken everything else.
-                )
-            ),
-            editorSession.previewReferenceInstant,
-            complicationsPreviewData
-        )
 
         val showDivisionRingStyle =
             userStyle[showDivisionRing] as UserStyleSetting.BooleanUserStyleSetting.BooleanOption
